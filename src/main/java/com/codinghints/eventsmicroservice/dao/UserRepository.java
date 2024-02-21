02f0c33ef828711c05463b60ff454a9cf287eac9
Ayubu Kayange <kayangejr3@gmail.com>
2024-02-21 10:56:55 +0300

    Repository components

--- src/main/java/com/codinghints/eventsmicroservice/dao/UserRepository.java
+++ src/main/java/com/codinghints/eventsmicroservice/dao/UserRepository.java
@@ -2,8 +2,13 @@ package com.codinghints.eventsmicroservice.dao;
 
 import com.codinghints.eventsmicroservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsernameContainingIgnoreCase(String john);
+
+    Optional<User> findByEmail(String email);
 }
